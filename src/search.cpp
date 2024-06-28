f9fbf71ac7b5cc21fd8cccf326be20990eb51dfe
Daniel Monroe <39802758+Ergodice@users.noreply.github.com>
2024-06-28 14:24:21 -0400

    use min instead of clamp

--- src/search.cpp
+++ src/search.cpp
@@ -1391,7 +1391,7 @@ moves_loop:  // When in check, search starts here
         && !(bestValue >= beta && bestValue <= ss->staticEval)
         && !(!bestMove && bestValue >= ss->staticEval))
     {
-        auto bonus = std::clamp(int(bestValue - ss->staticEval) * std::clamp(depth, 0, 8) / 8,
+        auto bonus = std::clamp(int(bestValue - ss->staticEval) * std::min(depth, 8) / 8,
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] << bonus;
     }

b90c7def3e91f4612f2736e9349a0e14fe1b3afa
Daniel Monroe <39802758+Ergodice@users.noreply.github.com>
2024-07-21 21:07:46 -0400

    staticeval no longer undefined when in check so rm from pcm bench 1354193

--- src/search.cpp
+++ src/search.cpp
@@ -1363,9 +1363,9 @@ moves_loop:  // When in check, search starts here
     else if (!priorCapture && prevSq != SQ_NONE)
     {
         int bonus = (138 * (depth > 5) + 58 * (PvNode || cutNode) + 160 * ((ss - 1)->moveCount > 8)
-                     + 84 * (!ss->inCheck && bestValue <= ss->staticEval - 108)
-                     + 153 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 76)
-                     + 32 * (!(ss - 1)->inCheck && bestValue > -(ss - 1)->staticEval + 76));
+                     + 84 * (bestValue <= ss->staticEval - 108)
+                     + 153 * (bestValue <= -(ss - 1)->staticEval - 76)
+                     + 32 * (bestValue > -(ss - 1)->staticEval + 76));
 
         // Proportional to "how much damage we have to undo"
         bonus += std::clamp(-(ss - 1)->statScore / 100, -94, 300);

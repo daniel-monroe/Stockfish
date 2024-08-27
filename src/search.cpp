09029d79adebeaa1f83dfca30a518c45f2f00fed
Daniel Monroe <github.com@danielmonroe.net>
2024-08-26 20:26:05 -0400

    reduce on ttcaptures iff not capture bench 1726335

--- src/search.cpp
+++ src/search.cpp
@@ -1137,7 +1137,7 @@ moves_loop:  // When in check, search starts here
             r += 2 - (ttData.depth >= depth && ss->ttPv);
 
         // Increase reduction if ttMove is a capture (~3 Elo)
-        if (ttCapture)
+        if (ttCapture && !capture)
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)

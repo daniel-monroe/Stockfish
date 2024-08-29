252092a27936fdafb47f15fd93d2d4b96dc06e71
Daniel Monroe <github.com@danielmonroe.net>
2024-08-29 17:37:05 -0400

    simplify oppWorsening in fut margin to improving bench 1671861

--- src/search.cpp
+++ src/search.cpp
@@ -68,10 +68,9 @@ namespace {
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorsening) {
     Value futilityMult       = 122 - 37 * noTtCutNode;
-    Value improvingDeduction = improving * futilityMult * 2;
-    Value worseningDeduction = oppWorsening * futilityMult / 3;
+    Value improvingDeduction = improving * futilityMult * 7 / 3;
 
-    return futilityMult * d - improvingDeduction - worseningDeduction;
+    return futilityMult * d - improvingDeduction;
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {

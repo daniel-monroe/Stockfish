49b36f454bef2a0cd9cc7ae5590156be19b1ca01
Daniel Monroe <github.com@danielmonroe.net>
2024-09-02 14:04:13 -0400

    slower delta increase bench 1513617

--- src/search.cpp
+++ src/search.cpp
@@ -357,7 +357,7 @@ void Search::Worker::iterative_deepening() {
                 else
                     break;
 
-                delta += delta / 3;
+                delta += delta / 4;
 
                 assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
             }

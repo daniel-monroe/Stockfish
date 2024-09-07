d43f416421b71716bec81c4101599d82a6838880
Daniel Monroe <github.com@danielmonroe.net>
2024-09-06 23:46:32 -0400

    movepick coeff adjustments bench 1306217

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -152,11 +152,11 @@ void MovePicker::score() {
             Square    to   = m.to_sq();
 
             // histories
-            m.value = (*mainHistory)[pos.side_to_move()][m.from_to()];
+            m.value = 3 * (*mainHistory)[pos.side_to_move()][m.from_to()] / 2;
             m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
-            m.value += 2 * (*continuationHistory[0])[pc][to];
+            m.value += 3 * (*continuationHistory[0])[pc][to] / 2;
             m.value += (*continuationHistory[1])[pc][to];
-            m.value += (*continuationHistory[2])[pc][to] / 3;
+            m.value += 2 * (*continuationHistory[2])[pc][to] / 3;
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
 

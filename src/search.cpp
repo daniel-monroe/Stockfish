2a68d950c3eb05598d4c189340d63be641587a10
Daniel Monroe <github.com@danielmonroe.net>
2024-10-28 17:26:46 -0700

    mcp in qsearch take 1 bench 1240372

--- src/search.cpp
+++ src/search.cpp
@@ -1601,7 +1601,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2)
+                if (moveCount > 2 + (ss->staticEval > beta + 100))
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];

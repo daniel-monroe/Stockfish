5d2954658b01296f45799da4cf6957add6ff6b2c
Daniel Monroe <github.com@danielmonroe.net>
2024-11-01 11:00:19 -0700

    take 2 bench 1260013

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~4 Elo)
         if (cutNode)
-            r += 2 - (ttData.depth >= depth && ss->ttPv);
+            r += 2 - (ttData.depth >= depth && ss->ttPv) + (depth < 8);
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)

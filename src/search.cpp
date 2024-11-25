f50ce60413806da383f50b4840dbee43ee100899
Daniel Monroe <github.com@danielmonroe.net>
2024-11-24 18:39:17 -0800

    tune bench 959947

--- src/search.cpp
+++ src/search.cpp
@@ -68,7 +68,7 @@ namespace {
 
 int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0, a10 = 0;
 
-TUNE(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, SetRange(-1000, 1000));
+TUNE(SetRange(-1000, 1000) , a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
 
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorsening) {

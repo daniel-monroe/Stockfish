12de47f21b89b48659372f123ca0746fec6a6c4a
Daniel Monroe <github.com@danielmonroe.net>
2024-11-24 23:05:59 -0800

    bench

--- src/search.cpp
+++ src/search.cpp
@@ -1169,8 +1169,8 @@ moves_loop:  // When in check, search starts here
             r += 2518 - (ttData.depth >= depth && ss->ttPv) * 991;
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
-        if (ttCapture && !capture)
-            r += 1043 + (depth < 8) * 999;
+        if (ttCapture && move != ttData.move)
+            r += 1043 + (depth < 8 || bestMove == ttData.move) * 999;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)

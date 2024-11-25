6115e6b94e2b22175b1c8fe8463acfde34f3c4f7
Daniel Monroe <github.com@danielmonroe.net>
2024-11-24 22:29:50 -0800

    less reduction for known baller bench 931930

--- src/search.cpp
+++ src/search.cpp
@@ -1180,6 +1180,8 @@ moves_loop:  // When in check, search starts here
         else if (move == ttData.move)
             r -= 1879;
 
+        r -= 500 * (ttData.value >= beta + 100);
+
         if (capture)
             ss->statScore = 0;
         else

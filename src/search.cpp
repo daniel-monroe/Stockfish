2e0ef24ae0d429a8e49d73fcdcdf3aca075c1160
Daniel Monroe <github.com@danielmonroe.net>
2024-11-24 23:06:44 -0800

    take 1 bench 790957

--- src/search.cpp
+++ src/search.cpp
@@ -538,7 +538,7 @@ Value Search::Worker::search(
 
     // Dive into quiescence search when the depth reaches zero
     if (depth <= 0)
-        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
+        return qsearch < PvNode ? PV : NonPV > (pos, ss, alpha, beta);
 
     // Limit the depth if extensions made it too large
     depth = std::min(depth, MAX_PLY - 1);
@@ -1171,6 +1171,7 @@ moves_loop:  // When in check, search starts here
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && move != ttData.move)
             r += 1043 + (depth < 8 || bestMove == ttData.move) * 999;
+        ;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)

4321958f81a6da154bf9156e7e14e82b1549a0e1
Daniel Monroe <github.com@danielmonroe.net>
2024-11-27 08:18:40 -0800

    probcut ease based on tt depth bench 823940

--- src/search.cpp
+++ src/search.cpp
@@ -926,7 +926,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea (~4 Elo)
-    probCutBeta = beta + 417;
+    probCutBeta = beta + 550 - 50 * std::min(ttData.depth - depth + 4, 4);
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY)

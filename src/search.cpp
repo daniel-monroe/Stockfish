a6a737ec06c9a96f886dbc415fd7220decbf1701
Daniel Monroe <github.com@danielmonroe.net>
2024-11-27 21:11:01 -0800

    less stat update if excludedmove bench 1048864

--- src/search.cpp
+++ src/search.cpp
@@ -1375,7 +1375,7 @@ moves_loop:  // When in check, search starts here
     // If there is a move that produces search value greater than alpha,
     // we update the stats of searched moves.
     else if (bestMove)
-        update_all_stats(pos, ss, *this, bestMove, prevSq, quietsSearched, capturesSearched, depth);
+        update_all_stats(pos, ss, *this, bestMove, prevSq, quietsSearched, capturesSearched, excludedMove ? depth / 2 : depth);
 
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)

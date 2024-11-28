f0619076ce8c9b06163d967c9c8c4565c6c6a6d2
Daniel Monroe <github.com@danielmonroe.net>
2024-11-27 21:15:00 -0800

    simplify reduction bench 1066752

--- src/search.cpp
+++ src/search.cpp
@@ -1704,7 +1704,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
 Depth Search::Worker::reduction(bool i, Depth d, int mn, int delta) const {
     int reductionScale = reductions[d] * reductions[mn];
-    return (reductionScale + 1304 - delta * 814 / rootDelta) + (!i && reductionScale > 1423) * 1135;
+    return (reductionScale + 500 + 814 * (delta == 1)) + (!i && reductionScale > 1423) * 1135;
 }
 
 // elapsed() returns the time elapsed since the search started. If the

615c6a17b4e7527b8e921fe9a5fb9f89a7a78a70
Daniel Monroe <github.com@danielmonroe.net>
2024-12-07 21:04:07 -0800

    fix bug bench 959947

--- src/search.cpp
+++ src/search.cpp
@@ -450,8 +450,8 @@ void Search::Worker::iterative_deepening() {
             fallingEval = std::clamp(fallingEval, 0.580, 1.667);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
-            bool stableBestMove = lastBestMoveDepth + 8 >= completedDepth;
-            timeReduction       = stableBestMove ? 0.687 : 1.495; 
+            bool stableBestMove = lastBestMoveDepth + 8 < completedDepth;
+            timeReduction       = stableBestMove ? 1.495 : 0.687;
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
             double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
             double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;

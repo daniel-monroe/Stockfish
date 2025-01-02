8f1b5b57fbafa45b329f72fd3e878e5c41eaebfa
Daniel Monroe <github.com@danielmonroe.net>
2025-01-02 18:02:11 -0500

    a bench 1090900

--- src/search.cpp
+++ src/search.cpp
@@ -1108,7 +1108,7 @@ moves_loop:  // When in check, search starts here
 
                 // If the ttMove is assumed to fail high over current beta (~7 Elo)
                 else if (ttData.value >= beta)
-                    extension = -3;
+                    extension = -3 - (ttData.depth >= depth);
 
                 // If we are on a cutNode but the ttMove is not assumed to fail high
                 // over current beta (~1 Elo)

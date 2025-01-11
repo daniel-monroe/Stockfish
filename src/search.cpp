04f3c5baffae74a8106f9b82c3fcc1e7d5028efe
Daniel Monroe <github.com@danielmonroe.net>
2025-01-11 15:21:07 -0800

    a bench 999229

--- src/search.cpp
+++ src/search.cpp
@@ -982,7 +982,8 @@ moves_loop:  // When in check, search starts here
 
         int delta = beta - alpha;
 
-        Depth r = reduction(improving, depth, moveCount, delta);
+        Depth r = reduction(improving, depth, moveCount, delta,
+                            ss->staticEval > (ss - 2)->staticEval + 100);
 
         // Step 14. Pruning at shallow depth (~120 Elo).
         // Depth conditions are important for mate finding.
@@ -1710,9 +1711,9 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
     return bestValue;
 }
 
-Depth Search::Worker::reduction(bool i, Depth d, int mn, int delta) const {
+Depth Search::Worker::reduction(bool i, Depth d, int mn, int delta, bool i2) const {
     int reductionScale = reductions[d] * reductions[mn];
-    return reductionScale - delta * 814 / rootDelta + !i * reductionScale / 3 + 1304;
+    return reductionScale - delta * 814 / rootDelta + !i * reductionScale / 3 + 1304 - i2 * reductionScale / 6;
 }
 
 // elapsed() returns the time elapsed since the search started. If the

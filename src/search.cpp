5034c574a0fc93caf067f52ead0b43098e2d4ee7
Daniel Monroe <github.com@danielmonroe.net>
2025-01-11 17:26:31 -0800

    take 2 bench 1213878

--- src/search.cpp
+++ src/search.cpp
@@ -1033,6 +1033,9 @@ moves_loop:  // When in check, search starts here
                 Value futilityValue =
                   ss->staticEval + (bestValue < ss->staticEval - 45 ? 140 : 43) + 141 * lmrDepth;
 
+                if (bestMove)
+                    futilityValue -= 200;
+
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 12 && futilityValue <= alpha)
                 {

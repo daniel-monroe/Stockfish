5f943996750a564ef0021bd2d241ed565c7208cf
Daniel Monroe <github.com@danielmonroe.net>
2025-01-11 17:35:51 -0800

    less pv+no best move reduction take 2 bench 1384420

--- src/search.cpp
+++ src/search.cpp
@@ -1154,7 +1154,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r -= 1024;
+            r -= 1024 + 1024 * (!bestMove);
 
         // These reduction adjustments have no proven non-linear scaling
 

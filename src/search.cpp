731b5f7121d339a7be2474a35ca3a99f959fbff6
Daniel Monroe <github.com@danielmonroe.net>
2025-01-15 14:29:23 -0800

    take 3 bench 1488617

--- src/search.cpp
+++ src/search.cpp
@@ -795,7 +795,7 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node (~40 Elo)
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 14
-        && eval - futility_margin(depth - ss->reducing * (ss->virtualDepth <= 0), cutNode && !ss->ttHit, improving, opponentWorsening)
+        && eval - futility_margin(depth, cutNode && !ss->ttHit, improving || (ss->reducing && ss->virtualDepth <= 0), opponentWorsening)
                - (ss - 1)->statScore / 310
                + (ss->staticEval == eval) * (40 - std::abs(correctionValue) / 131072) 
              >= beta

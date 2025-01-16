8ddb74b575e21c4cc42e8589502d82923081daf0
Daniel Monroe <github.com@danielmonroe.net>
2025-01-16 11:49:02 -0800

    small lmr depth increase when entering a ttpv node bench 1283021

--- src/search.cpp
+++ src/search.cpp
@@ -782,6 +782,7 @@ Value Search::Worker::search(
     {
         depth++;
     }
+    depth = std::min(depth + std::max(priorReduction, 0), depth + ss->ttPv);
 
     // Step 7. Razoring (~1 Elo)
     // If eval is really low, check with qsearch if we can exceed alpha. If the

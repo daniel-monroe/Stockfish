e68e10508336ae76b6ebf414a07059df77992908
Daniel Monroe <github.com@danielmonroe.net>
2025-01-18 13:39:14 -0800

    idk bench 1379150

--- src/search.cpp
+++ src/search.cpp
@@ -1017,7 +1017,7 @@ moves_loop:  // When in check, search starts here
                 history += 2 * thisThread->mainHistory[us][move.from_to()];
 
                 lmrDepth += history / 3459;
-
+                //
                 Value futilityValue =
                   ss->staticEval + (bestValue < ss->staticEval - 47 ? 137 : 47) + 142 * lmrDepth;
 

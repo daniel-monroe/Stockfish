12cb144e10b7c0baf0fb2de664eac7481cdb7a99
Daniel Monroe <github.com@danielmonroe.net>
2025-01-19 11:15:48 -0800

    rebase on pr 5808 bench 2166782

--- src/search.cpp
+++ src/search.cpp
@@ -982,7 +982,8 @@ moves_loop:  // When in check, search starts here
         // Decrease reduction if position is or has been on the PV (~7 Elo)
         if (ss->ttPv)
             r -= 1037 + (ttData.value > alpha) * 965 + (ttData.depth >= depth) * 960
-               + 1024 * (PvNode && !bestMove);
+               + 1024 * (PvNode && !bestMove) + 16 * depth;
+        ;
 
         // Step 14. Pruning at shallow depth (~120 Elo).
         // Depth conditions are important for mate finding.

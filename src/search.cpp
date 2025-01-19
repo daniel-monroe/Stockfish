f9ea351cef77170122a96fddc573a761be1e1859
Daniel Monroe <github.com@danielmonroe.net>
2025-01-19 10:11:05 -0800

    ttpv red if we can get the lvl 2 lmr cap bench 1951399

--- src/search.cpp
+++ src/search.cpp
@@ -981,7 +981,8 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if position is or has been on the PV (~7 Elo)
         if (ss->ttPv)
-            r -= 1037 + (ttData.value > alpha) * 965 + (ttData.depth >= depth) * 960;
+            r -= 1037 + (ttData.value > alpha) * 965 + (ttData.depth >= depth) * 960
+               + 1024 * (PvNode && !bestMove);
 
         // Step 14. Pruning at shallow depth (~120 Elo).
         // Depth conditions are important for mate finding.

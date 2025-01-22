43d2f087b5c19a9a0af07981f1ff0d3ecaa46b0f
Daniel Monroe <github.com@danielmonroe.net>
2025-01-21 16:26:43 -0800

    move not the tt capture reduction bench 1514338

--- src/search.cpp
+++ src/search.cpp
@@ -1234,8 +1234,9 @@ moves_loop:  // When in check, search starts here
                 r += 2111;
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
-            value =
-              -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3444), !cutNode);
+            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
+                                   newDepth - (r > 3444) - (ttCapture && move != ttData.move),
+                                   !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,

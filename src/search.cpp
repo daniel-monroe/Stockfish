063a5d59df0c61c4b29b184f1ecd9a7508b0fc55
Daniel Monroe <github.com@danielmonroe.net>
2025-01-25 19:29:25 -0800

    ss based tt take 2 2150973

--- src/search.cpp
+++ src/search.cpp
@@ -1804,7 +1804,7 @@ void update_all_stats(const Position&      pos,
     Piece                  moved_piece    = pos.moved_piece(bestMove);
     PieceType              captured;
 
-    int bonus = stat_bonus(depth) + (300 + std::clamp(-bestSs/80, 0, 300)) * isTTMove;
+    int bonus = stat_bonus(depth) + (300 + std::clamp(-bestSs/50, 0, 500)) * isTTMove;
     int malus = stat_malus(depth);
 
     if (!pos.capture_stage(bestMove))

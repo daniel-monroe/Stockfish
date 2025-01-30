ae6d0c1a3ea0094f55c173342df06476d949b9b1
Daniel Monroe <github.com@danielmonroe.net>
2025-01-29 21:01:34 -0800

    lessfmc@ttcap bench 1980555

--- src/search.cpp
+++ src/search.cpp
@@ -986,7 +986,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-            if (moveCount >= futility_move_count(improving, depth))
+            if (moveCount >= futility_move_count(improving || (depth >= 3 && ttCapture), depth))
                 mp.skip_quiet_moves();
 
             // Reduced depth of the next LMR search

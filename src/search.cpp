07df790c7b33030632fb9f94405257d45d279071
Daniel Monroe <github.com@danielmonroe.net>
2025-01-30 18:04:49 -0800

    less pcm bonus if first ranked non tt move bench 1935009

--- src/search.cpp
+++ src/search.cpp
@@ -1381,7 +1381,7 @@ moves_loop:  // When in check, search starts here
         int bonusScale = (118 * (depth > 5) + 37 * !allNode + 169 * ((ss - 1)->moveCount > 8)
                           + 128 * (!ss->inCheck && bestValue <= ss->staticEval - 102)
                           + 115 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 82)
-                          + 80 * ((ss - 1)->isTTMove) + std::min(-(ss - 1)->statScore / 106, 318));
+                          + 80 * ((ss - 1)->isTTMove) + std::min(-(ss - 1)->statScore / 106, 318) - 80 * ((ss - 1)->moveCount == 1 && !(ss - 1)->isTTMove));
 
         bonusScale = std::max(bonusScale, 0);
 

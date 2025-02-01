15037511a8136d5d459d15427c07ef805b685956
Daniel Monroe <github.com@danielmonroe.net>
2025-02-01 10:04:34 -0800

    asdf bench 2100567

--- src/search.cpp
+++ src/search.cpp
@@ -864,7 +864,7 @@ Value Search::Worker::search(
         // If value from transposition table is lower than probCutBeta, don't attempt
         // probCut there and in further interactions with transposition table cutoff
         // depth is set to depth - 3 because probCut search has depth set to depth - 4
-        // but we also do a move before it. So effective depth is equal to depth - 3.
+        // but we also do a move before it. So effective depth is equal to depth - 3. 
         && !(is_valid(ttData.value) && ttData.value < probCutBeta))
     {
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);

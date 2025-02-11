0f5d5391369324626d4af34dffd6ac2ac0bd30f6
Daniel Monroe <github.com@danielmonroe.net>
2025-02-11 14:09:01 -0800

    take 2 bench 2332513

--- src/search.cpp
+++ src/search.cpp
@@ -669,7 +669,9 @@ Value Search::Worker::search(
     // to save indentation, we list the condition in all code between here and there.
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!PvNode && !excludedMove
+        && ttData.depth
+             > depth - (ttData.value <= beta) - (ttData.value <= beta && priorReduction > 0)
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || (depth > 9 || (rootDepth > 10 && depth > 5))))

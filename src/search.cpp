5e2ffa7089ba0fc7043478d57c6ced0b5efe6dce
Daniel Monroe <github.com@danielmonroe.net>
2025-04-06 19:13:19 -0700

    take 1 bench 1770038

--- src/search.cpp
+++ src/search.cpp
@@ -844,7 +844,7 @@ Value Search::Worker::search(
 
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188)
+    if ((ss-1)->moveCount > 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188)
         depth--;
 
     // Step 7. Razoring

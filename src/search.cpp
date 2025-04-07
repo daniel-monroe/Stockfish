a4e5e6140c34e8de1618fe4f9ca3feb29a47f779
Daniel Monroe <github.com@danielmonroe.net>
2025-04-06 18:47:08 -0700

    take 4 bench 1952863

--- src/search.cpp
+++ src/search.cpp
@@ -1089,7 +1089,8 @@ moves_loop:  // When in check, search starts here
                 lmrDepth += history / 3593;
 
                 Value futilityValue = ss->staticEval + (bestMove ? 48 : 146) + 116 * lmrDepth
-                                    + 103 * (bestValue < ss->staticEval - 128);
+                                    + 103 * (bestValue < ss->staticEval - 128) + 100 * ss->ttPv;
+                
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning

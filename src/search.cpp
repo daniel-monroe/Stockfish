d36ce320200de514c390973e50f40a8b27786bc1
Daniel Monroe <github.com@danielmonroe.net>
2025-04-17 18:42:30 -0700

    take 2 bench 1630502

--- src/search.cpp
+++ src/search.cpp
@@ -865,7 +865,7 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
-        && ss->staticEval >= beta - 19 * depth + 418 + 100 * (ttData.depth == DEPTH_UNSEARCHED) && !excludedMove
+        && ss->staticEval >= beta - 19 * depth + 418 - 100 * (ttData.depth == DEPTH_UNSEARCHED) && !excludedMove
         && pos.non_pawn_material(us)
         && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
     {

db78742273c06efc8f5255a93e088565da54090f
Daniel Monroe <github.com@danielmonroe.net>
2025-04-20 09:58:04 -0700

    take 3 bench 1767736

--- src/search.cpp
+++ src/search.cpp
@@ -1462,7 +1462,8 @@ moves_loop:  // When in check, search starts here
            + 141 * (!ss->inCheck && bestValue <= ss->staticEval - 100)
            + 121 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 75)
            + 86 * ((ss - 1)->isTTMove) + 86 * (ss->cutoffCnt <= 3)
-           + std::min(-(ss - 1)->statScore / 112, 303));
+           + std::min(-(ss - 1)->statScore / 112, 220)
+               * (1 + ((ss - 1)->statScore < 0 && (ss - 1)->isTTMove)));
 
         bonusScale = std::max(bonusScale, 0);
 

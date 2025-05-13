1ad1abfd77a2055f4691ea3df7023dce9a097e0a
Daniel Monroe <github.com@danielmonroe.net>
2025-05-12 18:54:48 -0700

    optimized after_key no depth condition bench 1866179

--- src/search.cpp
+++ src/search.cpp
@@ -739,7 +739,7 @@ Value Search::Worker::search(
         if (pos.rule50_count() < 90)
         {
 
-            if (depth >= 4 && ttData.move && pos.legal(ttData.move) && pos.pseudo_legal(ttData.move)
+            if (ttData.move && pos.legal(ttData.move) && pos.pseudo_legal(ttData.move)
                 && !is_decisive(ttData.value) && ttData.move.type_of() != EN_PASSANT
                 && ttData.move.type_of() != CASTLING && ttData.move.type_of() != PROMOTION)
             {

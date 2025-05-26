af80b4ab57308ab6815aab49243c197337099887
Daniel Monroe <github.com@danielmonroe.net>
2025-05-26 00:19:08 -0700

    take 1 bench 2480830

--- src/search.cpp
+++ src/search.cpp
@@ -1154,7 +1154,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth++;
+                depth+= 2;
             }
 
             // Multi-cut pruning

64664b57f453f891ab05738222d86220c6780822
Daniel Monroe <github.com@danielmonroe.net>
2025-06-03 12:07:34 -0700

    take 1 bench 2337029

--- src/search.cpp
+++ src/search.cpp
@@ -836,7 +836,7 @@ Value Search::Worker::search(
     // The depth condition is important for mate finding.
     {
         auto futility_margin = [&](Depth d) {
-            Value futilityMult = 93 - 20 * (cutNode && !ss->ttHit);
+            Value futilityMult = 93 - 20 * (cutNode && !ss->ttHit) + 40 * (ss-1)->isPvNode;
 
             return futilityMult * d                      //
                  - improving * futilityMult * 2          //

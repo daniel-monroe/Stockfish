1a69463d1b6787c85cc2933f5c8153720a239df6
Daniel Monroe <github.com@danielmonroe.net>
2025-06-07 19:35:23 -0700

    decrease reduction if uncertain bench 2307268

--- src/search.cpp
+++ src/search.cpp
@@ -1205,6 +1205,7 @@ moves_loop:  // When in check, search starts here
         r += 316;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 66;
         r -= std::abs(correctionValue) / 28047;
+        r -= uncertainty;
 
         // Increase reduction for cut nodes
         if (cutNode)

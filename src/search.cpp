a6d14c21138b47772abecf80e524f8ef755a3f48
Daniel Monroe <github.com@danielmonroe.net>
2025-06-10 23:03:17 -0700

    Simplify quiet move streak term bench 2155007

--- src/search.cpp
+++ src/search.cpp
@@ -1208,7 +1208,7 @@ moves_loop:  // When in check, search starts here
             r += 1036 + allNode * 848;
 
         if (!capture && !givesCheck && ss->quietMoveStreak >= 2)
-            r += (ss->quietMoveStreak - 1) * 50;
+            r += ss->quietMoveStreak * 50;
 
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)

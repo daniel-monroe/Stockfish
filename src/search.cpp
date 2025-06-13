2a2c727a0b52df56e0e830542dafa88f51a5f06a
Daniel Monroe <github.com@danielmonroe.net>
2025-06-13 09:44:15 -0700

    Clean up quiet move streak logic bench 1905627

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,7 @@ moves_loop:  // When in check, search starts here
         movedPiece = pos.moved_piece(move);
         givesCheck = pos.gives_check(move);
 
-        (ss + 1)->quietMoveStreak = (!capture && !givesCheck) ? (ss->quietMoveStreak + 1) : 0;
+        ss->quietMoveStreak = (!capture && !givesCheck) ? ((ss-1)->quietMoveStreak + 1) : 0;
 
         // Calculate new depth for this move
         newDepth = depth - 1;
@@ -1207,8 +1207,7 @@ moves_loop:  // When in check, search starts here
         if ((ss + 1)->cutoffCnt > 2)
             r += 1036 + allNode * 848;
 
-        if (!capture && !givesCheck && ss->quietMoveStreak >= 2)
-            r += (ss->quietMoveStreak - 1) * 50;
+        r += ss->quietMoveStreak * 50;
 
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)

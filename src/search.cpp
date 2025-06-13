545a98721838265e6423b2e7e30008725a6c4e97
Daniel Monroe <github.com@danielmonroe.net>
2025-06-13 11:24:24 -0700

    take 2 bench 2489928

--- src/search.cpp
+++ src/search.cpp
@@ -1204,7 +1204,7 @@ moves_loop:  // When in check, search starts here
             r += 1210 + (depth < 8) * 963;
 
         // Increase reduction if next ply has a lot of fail high
-        if ((ss + 1)->cutoffCnt > 2)
+        if ((ss + 1)->cutoffCnt > 2 && (ss - 1)->moveCount > 1)
             r += 1036 + allNode * 848;
 
         if (!capture && !givesCheck && ss->quietMoveStreak >= 2)

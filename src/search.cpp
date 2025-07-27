dcf3f5dcebe9aad7f141a0c9ed31be1d37736782
Daniel Monroe <github.com@danielmonroe.net>
2025-07-27 11:04:15 -0400

    take 3 bench 2659466

--- src/search.cpp
+++ src/search.cpp
@@ -1222,7 +1222,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, std::min(newDepth - r / 1024,
+            Depth d = std::max(PvNode ? depth / 2 : 1, std::min(newDepth - r / 1024,
                                            newDepth + !allNode + (PvNode && !bestMove)))
                     + PvNode;
 

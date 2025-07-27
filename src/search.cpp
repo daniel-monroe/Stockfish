41e7c8015d52b9948c297c4fc28d6cc440ad2ac4
Daniel Monroe <github.com@danielmonroe.net>
2025-07-27 10:12:41 -0400

    take 1 bench 2678829

--- src/search.cpp
+++ src/search.cpp
@@ -1038,7 +1038,7 @@ moves_loop:  // When in check, search starts here
                 // Futility pruning for captures
                 if (!givesCheck && lmrDepth < 7 && !ss->inCheck)
                 {
-                    Value futilityValue = ss->staticEval + 232 + 224 * lmrDepth
+                    Value futilityValue = ss->staticEval + 232 * (move.to_sq() == prevSq) + 224 * lmrDepth
                                         + PieceValue[capturedPiece] + 131 * captHist / 1024;
                     if (futilityValue <= alpha)
                         continue;

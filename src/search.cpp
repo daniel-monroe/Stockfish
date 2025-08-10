848d3013d5939ca03abfc4ea3f434c95396d2287
Daniel Monroe <github.com@danielmonroe.net>
2025-08-10 15:10:25 -0400

    take 1 bench 3322498

--- src/search.cpp
+++ src/search.cpp
@@ -1363,7 +1363,7 @@ moves_loop:  // When in check, search starts here
                 }
 
                 // Reduce other moves if we have found at least one score improvement
-                if (depth > 2 && depth < 16 && !is_decisive(value))
+                if (PvNode && depth > 2 && depth < 16 && !is_decisive(value))
                     depth -= 2;
 
                 assert(depth > 0);

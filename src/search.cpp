5717d21db3cc94c1b6f1db062debc82ddf4df7b2
Daniel Monroe <github.com@danielmonroe.net>
2025-08-16 12:17:33 -0700

    a bench 3232247

--- src/search.cpp
+++ src/search.cpp
@@ -1361,7 +1361,7 @@ moves_loop:  // When in check, search starts here
                 }
 
                 // Reduce other moves if we have found at least one score improvement
-                if (depth > 2 && depth < 16 && !is_decisive(value))
+                if (depth > 2 && depth < 16 && !is_decisive(value) && move == ttData.move)
                     depth -= 2;
 
                 assert(depth > 0);

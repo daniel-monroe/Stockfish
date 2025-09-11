fefa4bb0faaabfe8fe8eea49bd9ce0802aed796c
Daniel Monroe <github.com@danielmonroe.net>
2025-09-10 23:55:23 -0400

    take 1 bench 2722155

--- src/history.h
+++ src/history.h
@@ -45,7 +45,7 @@ static_assert((CORRECTION_HISTORY_SIZE & (CORRECTION_HISTORY_SIZE - 1)) == 0,
               "CORRECTION_HISTORY_SIZE has to be a power of 2");
 
 inline int pawn_history_index(const Position& pos) {
-    return pos.pawn_key() & (PAWN_HISTORY_SIZE - 1);
+    return 0;
 }
 
 inline int pawn_correction_history_index(const Position& pos) {

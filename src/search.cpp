83f2fa2505dc5e3b1471b80b50b177d23f35cead
Daniel Monroe <github.com@danielmonroe.net>
2025-09-30 01:25:18 -0400

    a bench 2515619

--- src/search.cpp
+++ src/search.cpp
@@ -855,7 +855,7 @@ Value Search::Worker::search(
         };
 
         if (!ss->ttPv && depth < 14 && eval - futility_margin(depth) >= beta && eval >= beta
-            && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
+            && (!ttData.move || pos.capture_stage(ttData.move)) && !is_loss(beta) && !is_win(eval))
             return (2 * beta + eval) / 3;
     }
 

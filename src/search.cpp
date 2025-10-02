2b329405d183a9c61c3b329dc2366e7de1a50b5c
Daniel Monroe <github.com@danielmonroe.net>
2025-10-02 16:09:35 -0400

    b bench 2290807

--- src/search.cpp
+++ src/search.cpp
@@ -1408,13 +1408,13 @@ moves_loop:  // When in check, search starts here
         const int scaledBonus = std::min(144 * depth - 92, 1365) * bonusScale;
 
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
-                                      scaledBonus * 400 / 32768);
+                                      scaledBonus * 400 / 16384);
 
-        mainHistory[~us][((ss - 1)->currentMove).from_to()] << scaledBonus * 220 / 32768;
+        mainHistory[~us][((ss - 1)->currentMove).from_to()] << scaledBonus * 220 / 16384;
 
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             pawnHistory[pawn_history_index(pos)][pos.piece_on(prevSq)][prevSq]
-              << scaledBonus * 1164 / 32768;
+              << scaledBonus * 1164 / 16384;
     }
 
     // Bonus for prior capture countermove that caused the fail low

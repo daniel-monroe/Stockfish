f0e2d13ad400a042160a6ff4003540e91d0da3ea
Daniel Monroe <github.com@danielmonroe.net>
2025-10-04 06:04:29 -0400

    b bench 2531359

--- src/search.cpp
+++ src/search.cpp
@@ -1246,6 +1246,9 @@ moves_loop:  // When in check, search starts here
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
                                    newDepth - (r > 3212) - (r > 4784 && newDepth > 2), !cutNode);
+
+            if (value >= alpha && r > 5000)
+                value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, false);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,

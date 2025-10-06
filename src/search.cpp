a73f8ae70625b071886f2aa66b262c3ed0217401
Daniel Monroe <github.com@danielmonroe.net>
2025-10-06 18:55:53 -0400

    take 3 bench 2646667

--- src/search.cpp
+++ src/search.cpp
@@ -682,8 +682,7 @@ Value Search::Worker::search(
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 5)
         // avoid a TT cutoff if the rule50 count is high and the TT move is zeroing
-        && (depth > 8 || ttData.move == Move::none() || pos.rule50_count() < 80
-            || (!ttCapture && type_of(pos.moved_piece(ttData.move)) != PAWN)))
+        && (depth > 8 || !ttData.move || pos.rule50_count() < 80))
     {
         // If ttMove is quiet, update move sorting heuristics on TT hit
         if (ttData.move && ttData.value >= beta)

7f06e803d96ae2b9105f33c06bb2ccb9d3f37c30
Daniel Monroe <github.com@danielmonroe.net>
2025-12-16 16:07:21 -0500

    take 3 bench 2464392

--- src/search.cpp
+++ src/search.cpp
@@ -89,7 +89,7 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
                     + (*(ss - 4)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                  : 8;
 
-    return 12347 * pcv + 10821 * micv + 13168 * (wnpcv + bnpcv) + 9841 * cntcv;
+    return 13347 * pcv + 11821 * micv + 14168 * (wnpcv + bnpcv) + 10841 * cntcv;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation

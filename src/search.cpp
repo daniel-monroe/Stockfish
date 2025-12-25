9f8d2b5a22380e56db25ef86c996974c753285d8
Daniel Monroe <github.com@danielmonroe.net>
2025-12-25 15:28:32 -0500

    take 1 bench 3087250

--- src/search.cpp
+++ src/search.cpp
@@ -749,10 +749,13 @@ Value Search::Worker::search(
     opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
 
     // Hindsight adjustment of reductions based on static evaluation difference.
-    if (priorReduction >= 3 && !opponentWorsening)
-        depth++;
-    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
-        depth--;
+    if (!ss->inCheck && !excludedMove)
+    {
+        if (priorReduction >= 3 && !opponentWorsening)
+            depth++;
+        if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173 && !ss->ttPv)
+            depth--;
+    }
 
     // At non-PV nodes we check for an early TT cutoff
     if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
